ef55a4565f40b18619df1d9efad88270bd9b960f
Muhammad Umair <toplinegeeks@gmail.com>
2023-08-04 00:23:06 +0500

    update pages

---
+++ script.js
@@ -0,0 +1,6 @@
+// // Parallax effect for background image
+// window.addEventListener('scroll', () => {
+//     const scrollY = window.scrollY;
+//     const parallaxElement = document.querySelector('.header');
+//     parallaxElement.style.backgroundPositionY = `${scrollY * 0.7}px`;
+//   });
